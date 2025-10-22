276eb46098811bc0e54bc74489eab91955903fb8
Teacher-Yien <sothyien@gmail.com>
2025-10-22 11:45:40 +0700

    sd

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+import os
+from flask import send_from_directory
 
 app = create_app()
+
+@app.route('/static/<path:filename>')
+def custom_static(filename):
+    return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
